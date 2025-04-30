2d056746469ac47074e2f8c1f856583ac0a452e2
DobesDaniel <dobes.daniel@outlook.cz>
2025-04-30 18:07:08 +0200

    Update hyperparameters.py

--- SEATBELT_TRAIN_TEST/hyperparameters.py
+++ SEATBELT_TRAIN_TEST/hyperparameters.py
@@ -3,7 +3,7 @@
 input_size = 4   # features 2|4
 hidden_size = 64 # 512, 256, 128, 64
 num_classes = 5         # 5
-num_layers = 4  # 2
+num_layers = 4  # 1, 2, 4
 batch_size = 32
 num_epochs = 50        # 30, 50, 100
 learning_rate = 0.001   # 0.001, 1e-4
